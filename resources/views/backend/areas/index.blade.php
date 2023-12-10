e59865bd903148f5e68621152d24529eda13450a
AriefAnas1 <ariefanas085@gmail.com>
2023-12-10 09:12:50 +0700

    tampilan view areas

---
+++ resources/views/backend/areas/index.blade.php
@@ -0,0 +1,47 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', 'Areas  | ' . Config::get('adminlte.title'))
+
+@section('content_header')
+    <h1>Area</h1>
+@stop
+
+@section('content')
+    {{--Show message if any--}}
+    @include('layouts.flash-message')
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Daftar</h3>
+        </div>
+
+        <div class="card-body">
+            <div class="table-responsive">
+                {!! $html->table(['class' => 'table table-hover']) !!}
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link href="{{ asset('vendor/datatables/css/dataTables.bootstrap4.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/datatables-plugins/buttons/css/buttons.bootstrap4.css') }}" rel="stylesheet">
+@stop
+
+@section('js')
+    <!--Data tables-->
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/jszip/jszip.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/pdfmake/pdfmake.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/pdfmake/vfs_fonts.js') }}"></script>
+    {{--Button--}}
+    <script src="{{ asset('vendor/datatables-plugins/buttons/js/dataTables.buttons.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/buttons/js/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/buttons/js/buttons.colVis.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/buttons/js/buttons.html5.js') }}"></script>
+    <script src="{{ asset('vendor/datatables-plugins/buttons/js/buttons.print.js') }}"></script>
+    {!! $html->scripts() !!}
+    <script src="{{ asset('js/main_index.js'). '?v=' . rand(99999,999999) }}"></script>
+@stop
